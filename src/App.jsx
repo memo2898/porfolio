786d925a66cd2c35b7b89472c8479e258c89ca88
Manuel Maldonado <manuelmaldonado2898@gmail.com>
2024-08-27 11:16:43 -0400

    continuando portafolio

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,32 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import Home from './pages/home/Home'
+import About from './pages/about/About'
+import Portfolio from './pages/portfolio/Portfolio'
+import Login from './pages/login/Login'
+
+
 
 function App() {
 
 
   return (
-    <>
-      
-     
-    </>
+    
+  <>
+  <BrowserRouter>
+      <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+
+          <Route path="/login" element={<Login />} />
+
+          <Route path="*" element={<div>Esto debe ser una pagina 404</div>} />
+      </Routes>
+  </BrowserRouter>
+  </>
   )
 }
 
