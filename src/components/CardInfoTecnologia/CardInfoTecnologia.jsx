221652667525021711aa040bd0e253f757a070ea
Manuel Maldonado <manuelmaldonado2898@gmail.com>
2024-08-29 13:52:42 -0400

    Skills front reaizado

--- src/components/CardInfoTecnologia/CardInfoTecnologia.jsx
+++ src/components/CardInfoTecnologia/CardInfoTecnologia.jsx
@@ -1,9 +1,62 @@
+/* eslint-disable react/prop-types */
 
+import { useState } from 'react';
 import './CardInfoTecnologia.css'
-function cardInfoTecnologia({color, logoSVG, tecnologia, experiencia, ...props}) {
+
+
+function CardInfoTecnologia({color, imagen, imagenHover, tecnologia, experiencia, ...props}) {
+
+  const stylesHover = {
+    padding:"14px",
+    paddingLeft: "26px",
+    paddingRight: "26px",
+ 
+    borderRadius: '6px',
+    border:`solid 4px ${color}`
+  };
+
+const stylesLeave ={
+    padding:"18px",
+    paddingLeft: "30px",
+    paddingRight: "30px"
+}
+const [isHoverStyle, setIsHoverStyle] = useState(stylesLeave)
+
+const [imagenMostrar, setImagenMostrar] = useState(imagen)
+
+const efectosHover = ()=>{
+  
+  setIsHoverStyle(stylesHover)
+  setImagenMostrar(imagenHover)
+}
+const efectosHoverLeave = ()=>{
+
+  setIsHoverStyle(stylesLeave)
+  setImagenMostrar(imagen)
+}
+
+
+  
   return (
-    <div className="card_info_tecnologia hexagon" {...props}>sss</div>
+    <div style={isHoverStyle}>
+      <div className="card_info_tecnologia" {...props} onMouseOver={efectosHover} onMouseOut={efectosHoverLeave}>
+        <div className="cont-img-skill">
+          <img src={imagenMostrar}/>
+        </div>
+
+        
+        <div className='cont-text-descriptivo'>
+          <span className='text-tecnologia'>{tecnologia}</span>
+        </div>
+        <div className='cont-text-descriptivo'>
+          <span className='texto-experiencia'>{experiencia}</span>
+        </div>
+
+
+
+      </div>
+    </div>
   )
 }
 
-export default cardInfoTecnologia
+export default CardInfoTecnologia
--- src/components/secciones/seccion_skills/SeccionSkills.jsx
+++ src/components/secciones/seccion_skills/SeccionSkills.jsx
@@ -1,9 +1,44 @@
 
 import CardInfoTecnologia from "../../CardInfoTecnologia/CardInfoTecnologia"
 import SkeletonCardsSkills from "./SkeletonCardsSkills"
-
+import techEjemplo from "../../../assets/SVG/ejemploTech.svg"
+import techEjemplo2 from "../../../assets/SVG/ejemploTech2.svg"
+import { useEffect, useState } from "react"
 
 function SeccionSkills() {
+    const [initial, setInitial] = useState(false);
+    const [infoSkills, setInfoSkills] = useState([]);
+
+    useEffect(()=>{
+
+
+        //False request :
+        setTimeout(()=>{
+
+
+            const dataRespuesta = []
+            const cantidadDatos = 10
+            for (let i = 0; i < cantidadDatos; i++) {
+                dataRespuesta.push(     {
+                    id: i+1,
+                    imagen:techEjemplo,
+                    imagenHover:techEjemplo2,
+                    color:"#E35027",
+                    tecnologia:"HTML",
+                    experiencia:"+ 2 años"
+                })
+               
+                
+            }
+            
+            setInfoSkills(dataRespuesta)
+
+            
+            setInitial(true)
+        },2000)
+
+
+    },[])
   return (
     <section className="seccionSkills">
         <div className="margenes_general ">
@@ -13,11 +48,31 @@ function SeccionSkills() {
             </div>
            {/**Cards tecnologias start */}
                 <div className="cont-cards_tecnologias">
-                        
-                        <SkeletonCardsSkills/>
+                <div className="cont-cardsSkills">
 
-                        
-                        <CardInfoTecnologia/>
+
+                    {!initial? 
+                        <SkeletonCardsSkills/> 
+                    :
+                    
+                    infoSkills.map(skill =>{
+                        return(
+                          
+                            <CardInfoTecnologia 
+                            key={skill.id}
+                            imagen={skill.imagen}
+                            imagenHover={skill.imagenHover}
+                            color={skill.color}
+                            tecnologia={skill.tecnologia}
+                            experiencia={skill.experiencia}
+                            onClick={()=>{alert("Click en la card")}}
+                            />)
+                    })
+              
+                    
+                    
+                    }
+</div>
                 </div>
            {/**Cards tecnologias end */}
         </div>
