b1cf045bc926d79290321edea9cfb97b6a645d7a
Manuel Maldonado <manuelmaldonado2898@gmail.com>
2024-09-01 00:55:25 -0400

    Bloque Experciencia Listo... Ahora debo hacer el skeleton

--- src/components/bloqueExperiencia/BloqueExperiencia.jsx
+++ src/components/bloqueExperiencia/BloqueExperiencia.jsx
@@ -1,1 +1,68 @@
+/* eslint-disable react/prop-types */
 import './BloqueExperiencia.css'
+
+
+
+function BloqueExperiencia({year, posicion,institucion,pais,descripcion}) {
+  
+
+    
+  return (
+    <div className='bloqueExperiencia'>
+        
+        {/** //? Columna Principal 1 start*/}
+        <div className="columna-principal1-BE">
+            <div className="arriba_BE">
+
+                <div className="cont-item-year-mobile-arriba">
+                    <div className="cont-item-year-mobile">
+                        <span className='item-year'>{year}</span>
+                    </div>
+                </div>
+
+            </div>
+            <div className="abajo_BE">            
+                <div className="cont-item-year">
+                    <span className='item-year'>{year}</span>
+                </div>
+
+                <div className="cont-burbuja_liena">
+                    <div className="cont-burbuja">
+                        <div className="burbuja"></div>
+                    </div>
+                    {/* <div className="cont-linea-BE">
+                        <div className="linea-BE"></div>
+                    </div> */}
+                </div>
+
+            </div>
+
+            <div className="cont-linea-BE">
+                <div className="linea-BE"></div>
+            </div>
+            
+
+        </div>
+        {/** //? Columna Principal 1 end*/}
+
+        {/** //? Columna Principal 2 start*/}
+        <div className="columna-principal2-BE">
+            <div className="cont-titulo-principal2-B2">
+                <h2 className="titulo-principal2-B2">{posicion}</h2>
+            </div>
+
+            <div className="cont-texto-inst-pais-B2">
+                <p className="texto-inst-pais-B2">{institucion}, {pais}</p>
+            </div>
+            
+            <div className='cont-texto-descripcion-B2'>
+                <p className='texto-descripcion-B2'>{descripcion}</p>
+            </div>
+        </div>
+        {/** //? Columna Principal 2 end*/}
+
+    </div>
+  )
+}
+
+export default BloqueExperiencia
--- src/components/secciones/seccion_experience/SeccionExperience.jsx
+++ src/components/secciones/seccion_experience/SeccionExperience.jsx
@@ -1,13 +1,63 @@
 
+import BloqueExperiencia from '../../bloqueExperiencia/BloqueExperiencia';
+import './SeccionExperience.css';
+
 function SeccionExperience() {
+
     
   return (
     <div className="style-fondo-3">
-          <div className="margenes_general ">
+        <div className="margenes_general ">
           <div className="cont-titulo-principal">
                 <h1 className="titulo-principal">Experience</h1>
             </div>
-          </div>
+
+
+              <div className="cont-bloques-experiencias">
+              <BloqueExperiencia 
+                year={2005}
+                posicion={"Ingeniero de Software"}
+                institucion={"Banco Nacional"}
+                pais={"Rep Dominicana"}
+                descripcion={`Desarrollé e implementé soluciones bancarias personalizadas que mejoraron la eficiencia de las operaciones diarias. Participé en todas las fases del ciclo de vida del desarrollo de software, desde la recopilación de requisitos hasta la implementación final. Colaboré con un equipo multifuncional para optimizar la arquitectura de software y garantizar la calidad del código.`}
+              />
+
+<BloqueExperiencia 
+  year={2010}
+  posicion={"Analista de Sistemas"}
+  institucion={"Ministerio de Educación"}
+  pais={"Rep Dominicana"}
+  descripcion={`Responsable del análisis y diseño de sistemas de información para mejorar los procesos educativos. Realicé estudios de viabilidad, diseñé diagramas de flujo de datos y colaboré en la creación de planes de implementación. Trabajé estrechamente con los usuarios finales para garantizar que las soluciones desarrolladas cumplían con sus necesidades y expectativas.`}
+/>
+
+<BloqueExperiencia 
+  year={2015}
+  posicion={"Líder de Proyecto"}
+  institucion={"Empresa de Tecnología Global"}
+  pais={"Rep Dominicana"}
+  descripcion={`Encabezé proyectos de desarrollo de software a gran escala, coordinando equipos de desarrollo, asegurando la entrega dentro de los plazos y presupuestos establecidos. Implementé metodologías ágiles para mejorar la colaboración y la eficiencia del equipo. Supervisé la calidad del producto final y gestioné la comunicación con los stakeholders.`}
+/>
+
+<BloqueExperiencia 
+  year={2018}
+  posicion={"Consultor Técnico"}
+  institucion={"Consultora Internacional"}
+  pais={"Rep Dominicana"}
+  descripcion={`Brindé asesoría técnica a empresas en la implementación de soluciones de software avanzadas. Evalué las necesidades tecnológicas de los clientes y diseñé soluciones personalizadas para optimizar su infraestructura tecnológica. Además, impartí formación a equipos internos sobre nuevas herramientas y metodologías de desarrollo.`}
+/>
+
+<BloqueExperiencia 
+  year={2022}
+  posicion={"Arquitecto de Soluciones"}
+  institucion={"Empresa de Innovación Digital"}
+  pais={"Rep Dominicana"}
+  descripcion={`Diseñé arquitecturas de software complejas que soportan las necesidades estratégicas de la empresa. Colaboré con equipos de desarrollo para implementar soluciones escalables y seguras. Lideré la investigación y adopción de nuevas tecnologías para asegurar que las soluciones desarrolladas estuvieran a la vanguardia de la innovación.`}
+/>
+
+               
+              </div>
+        </div>
+
     </div>
   )
 }
